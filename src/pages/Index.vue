4a177b9e01fb4446d890239c2776c2aa86ca8744
nb9960 <nbodani8@gmail.com>
2021-06-01 22:29:45 +0530

    feat: added about section

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -2,30 +2,48 @@
   <Layout>
     <b-card title="Hi!" class="shadow-sm">
       <hr>
+      <p>I’m an Undergraduate at the  <strong><a target="_blank" href="https://www.iitbhu.ac.in/">Indian Institute of Technology (BHU), Varanasi</a></strong>​. 
+      An enthusiastic full-stack developer at my college’s premier coding club, 
+      <strong><a target="_blank" href="https://www.copsiitbhu.co.in/">Club of Programmers</a></strong>​.
+      </p>
       <p>
-        I have always liked making things, this became a professional career
-        eventually.
+        I started this journey by learning basics of web development in my school time and then after joining 
+        college, I started learning C/C++ and Programming concepts like Data Structures and Algorithms alongwith enhancing my knowledge in the field
+         of development. To dive deeper into the world of development, I participated in COPS summer of code'20 
+         from there I got selected in Club of Programmers thereby increasing my love for Open Source as I used to 
+         see my fellow developers contributing in various organizations. I am proficient in C/C++, Python, Javascript 
+         and Dart, I also have good knowledge of Ubuntu OS and Visual Studio Code.
+      </p>
+      <p>
+      I enjoy competitive programming, software development, attending hackathons and working on challenging 
+      projects. I have been involved in software development and have grown to like open source from the past 
+      year, because of its vibrant and engaging community. I actively use open-source software and like to 
+      contribute back when I can. 
+      </p>
+      <p>
+      I constantly strive to improve my skills and find new ways of thinking &amp; problem solving to stay on top of 
+      recent technological advancements. I prefer to give more importance to practical implementations rather than 
+      theoretical studies. I believe in solving problems by reusing the latest technological stuff, rather than 
+      reinventing the wheel. I have been working in a startup for a year now and have come to appreciate the power 
+      of teamwork.
       </p>
 
       <p>
-        I like turning ideas into software products. I started this journey by
-        teaching myself programming with C and Python. Then Java. Eventually I received a <strong> master's degree</strong> after studying in Sabanci University, where I took <strong> advanced Java, frontend development, Pyhton, machine learning and big data processing </strong> lectures.
-      </p>
-
-      <p>
-        I am currently working as the manager of
+        I am currently working as the Student Developer for
         <strong
-          ><a target="_blank" href="http://cospace.sabanciuniv.edu/"
-            >Collaboration Space</a
+          ><a target="_blank" href="https://www.ccextractor.org"
+            >CCExtractor Development</a
           >
         </strong>
-        in Sabanci University. Collaboration Space is a university makerspace,
-        where users make, think, develop and learn together using software and equipment like
-        3D printers, virtual reality, laser cutter, CNC machines and so on.
+        under <strong>Google Summer of Code</strong> for a project named <strong
+          ><a target="_blank" href="https://github.com/CCExtractor/beacon/"
+            >Beacon</a
+          >
+        </strong>
       </p>
       <p>
-        Makerspaces are interdisciplinary environments, where I had the
-        opportunity to be included in both hardware and software projects. I am lecturing introductory courses in Java and Python languages to both university and high school students.
+        Alongwith all this I am inquistive about investing in Stock Market and Cryptocurrency. 
+        Also, I like to work, travel, play games (both indoor &amp; outdoor), and listen to music.
       </p>
       
       <p>
@@ -54,13 +72,11 @@
         profile.
       </p>
       <p>
-        So that’s all for me. Now, you
-        <strong><g-link to="/contact/">tell me more</g-link></strong> about
-        yourself.
+        So that’s all from my side. I would love to hear from you, be it a suggestion, feedback, 
+        query or criticism. I like the idea of having a discussion. So, feel free to contact me using any social 
+        handle that I have provided or simply using
+        <strong><g-link to="/contact/">this</g-link></strong>. I would also love to hear your side of story.
       </p>
-    
-        
-        
     </b-card>
   </Layout>
 </template>
@@ -82,8 +98,3 @@ export default {
 
 }
 </script>
-
-<style lang="scss">
-
-
-</style>
